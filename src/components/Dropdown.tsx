507bd1598fdbbd5d978d7a95d2fc4e19f89bde72
minzx23 <144357990+minzx23@users.noreply.github.com>
2026-04-02 13:26:31 +0900

    [#15] Feat/15/공통컴포넌트 드롭다운 (#18)
    * 💄 Design: 드롭다운 스크롤바 디자인 변경
    
    * ✨ Feat: 공통 컴포넌트 Dropdown 구현
    
    * ♻️ Refactor: cn함수 사용하는 코드로 변경
    
    * 💄 Design: 드롭다운 컴포넌트 스크롤바 디자인 수정
    
    * ✨ Feat: 말줄임표 범위 상세 조절용 truncateText 유틸 함수 구현
    
    * ✨ Feat: md 사이즈 감지용 훅 useMediaQuery 구현
    
    * 💄 Design: 디테일 수정 및 프로필 드롭다운 재구성

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,107 @@
+import { useState } from 'react';
+import useMediaQuery from '@/hooks/useMediaQuery';
+import { cn } from '@/utils/cn';
+import { truncateText } from '@/utils/truncateText';
+import Icon from './Icon';
+
+interface DropdownProps {
+  options: string[];
+  onSelect: (value: string) => void;
+  placeholder?: string;
+  value?: string;
+  variant?: 'default' | 'profile' | 'status';
+  open?: boolean;
+  setOpen?: (open: boolean) => void;
+  classname?: string;
+}
+
+export default function Dropdown({
+  options,
+  onSelect,
+  placeholder,
+  value,
+  variant = 'default',
+  open: outerOpen,
+  setOpen: setOuterOpen,
+  classname,
+}: DropdownProps) {
+  const [innerOpen, setInnerOpen] = useState(false);
+  const open = variant === 'default' || variant === 'status' ? innerOpen : outerOpen;
+  const setOpen = variant === 'default' || variant === 'status' ? setInnerOpen : setOuterOpen;
+  const isMd = useMediaQuery('(min-width: 768px)');
+
+  const optionList = () => (
+    <ul
+      className={cn(
+        'dropdownScrollbar text-black-200 absolute mt-0.75 box-border truncate rounded-md border border-gray-300 px-0.75 md:mt-1 md:px-1',
+        options.length > 4 ? 'max-h-36.25 overflow-y-auto md:max-h-45.5' : 'h-auto',
+        variant === 'profile'
+          ? 'right-0 w-23.5 translate-x-7 md:left-0 md:w-27.5 md:translate-x-0'
+          : 'left-0 w-full',
+      )}
+    >
+      {options.map((opt) => {
+        const isSelected = value === opt;
+        return (
+          <li
+            key={opt}
+            onClick={() => {
+              onSelect(opt);
+              setOpen!(false);
+            }}
+            className={cn(
+              'mt-0.75 box-border flex cursor-pointer items-center justify-center truncate rounded-md text-sm leading-6.5 last:mb-0.75 md:mt-1 md:text-lg md:last:mb-1',
+              isSelected ? 'bg-blue-100 text-blue-200' : 'hover:bg-blue-100 hover:text-blue-200',
+              variant === 'profile' ? 'h-10.5 md:h-12' : 'h-8 md:h-10',
+            )}
+          >
+            <span title={opt} className={cn(variant === 'profile' ? '' : 'md:truncate')}>
+              {variant === 'profile' ? opt : truncateText(opt, isMd ? 5 : 4)}
+            </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+
+  return (
+    <div className={cn('relative inline-block')}>
+      {variant === 'profile' ? null : variant === 'status' ? (
+        <div
+          className={cn(
+            'flex h-8 cursor-pointer items-center justify-between gap-3.25 rounded-md border border-gray-300 px-2 text-sm leading-6.5 text-gray-400 md:h-10 md:gap-3.5 md:px-4 md:text-lg',
+            classname,
+          )}
+          onClick={() => setOpen!(!open)}
+        >
+          <span className={cn('w-5.75 truncate md:w-7')}>
+            {truncateText(value || placeholder || '', isMd ? 5 : 4)}
+          </span>
+          <Icon
+            name='arrowDown'
+            size={16}
+            className={cn('text-black-200 shrink-0 md:h-6.5 md:w-6.5')}
+          />
+        </div>
+      ) : (
+        <div
+          className={cn(
+            'flex h-8 cursor-pointer items-center justify-between gap-1.5 rounded-md border border-gray-300 px-2 text-sm leading-6.5 text-gray-400 md:h-10 md:gap-0 md:px-4 md:text-lg',
+            classname,
+          )}
+          onClick={() => setOpen!(!open)}
+        >
+          <span className={cn('w-8.5 truncate md:w-10.5')}>
+            {truncateText(value || placeholder || '', isMd ? 5 : 4)}
+          </span>
+          <Icon
+            name='arrowDown'
+            size={16}
+            className={cn('text-black-200 shrink-0 md:h-6.5 md:w-6.5')}
+          />
+        </div>
+      )}
+      {open && optionList()}
+    </div>
+  );
+}
