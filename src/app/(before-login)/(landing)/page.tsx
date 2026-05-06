59104a9e50565d289bdd08c0034d53d8ec65e6af
LMS10 <ggb7410@naver.com>
2026-05-07 00:33:21 +0900

    🔥 Remove: 임시 로그아웃 버튼 제거

--- src/app/(before-login)/(landing)/page.tsx
+++ src/app/(before-login)/(landing)/page.tsx
@@ -1,10 +1,7 @@
-import TempLogoutButton from '@/components/TempLogoutButton';
-
 export default function Home() {
   return (
     <>
       <div>랜딩 페이지</div>
-      <TempLogoutButton />
     </>
   );
 }
