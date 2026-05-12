dd7601a62babc79a149a70fa8917611d1206516e
LMS10 <ggb7410@naver.com>
2026-05-12 18:48:49 +0900

    ✨ Feat: ApiError 클래스 및 isApiError 타입 가드 추가

---
+++ src/utils/apiError.ts
@@ -0,0 +1,10 @@
+export class ApiError extends Error {
+  status: number;
+  constructor(message: string, status: number) {
+    super(message);
+    this.name = 'ApiError';
+    this.status = status;
+  }
+}
+
+export const isApiError = (err: unknown): err is ApiError => err instanceof ApiError;
