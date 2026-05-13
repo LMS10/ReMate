1a8bb0bad464e721d74e7323c02ab43ec0277e55
LMS10 <ggb7410@naver.com>
2026-05-13 20:26:06 +0900

    ✨ Feat: 워크스페이스 통계/영수증 목록/내보내기 API 타입, 서비스, 쿼리 훅 추가

--- src/apis/receipt/receipt.queries.ts
+++ src/apis/receipt/receipt.queries.ts
@@ -1,11 +1,14 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useMutation, useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
 import { receiptService } from './receipt.service';
+import { ReceiptListParams } from './receipt.type';
 
 export const receiptKeys = {
   all: ['receipts'] as const,
   upload: ['receipts', 'upload'] as const,
   detail: (receiptId: number) => ['receipts', receiptId] as const,
+  stats: (workspaceId: number) => ['receipts', 'stats', workspaceId] as const,
+  list: (params: ReceiptListParams) => ['receipts', 'list', params] as const,
 };
 
 interface QueryOptions {
@@ -34,5 +37,38 @@ export const useGetReceiptDetail = (
     queryKey: receiptKeys.detail(receiptId),
     queryFn: () => receiptService.getReceiptDetail(receiptId, workspaceId, accessToken!),
     enabled: !!accessToken && !!receiptId && enabled,
+  });
+};
+
+export const useGetReceiptStats = (workspaceId: number) => {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken as string | undefined;
+
+  return useQuery({
+    queryKey: receiptKeys.stats(workspaceId),
+    queryFn: () => receiptService.getReceiptStats(workspaceId, accessToken!),
+    enabled: !!accessToken && !!workspaceId,
+    staleTime: 1000 * 60,
+  });
+};
+
+export const useGetReceiptList = (params: ReceiptListParams) => {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken as string | undefined;
+
+  return useQuery({
+    queryKey: receiptKeys.list(params),
+    queryFn: () => receiptService.getReceiptList(params, accessToken!),
+    enabled: !!accessToken && !!params.workspaceId,
+    placeholderData: keepPreviousData,
+  });
+};
+
+export const useExportReceipts = () => {
+  const { data: session } = useSession();
+  const accessToken = session?.accessToken as string | undefined;
+
+  return useMutation({
+    mutationFn: (workspaceId: number) => receiptService.exportReceipts(workspaceId, accessToken!),
   });
 };
